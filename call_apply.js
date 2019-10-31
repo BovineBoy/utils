5614c6502b53f115fd35cf73586520c14afe38a5
bovine <bovine@kingdee.com>
2019-10-31 14:13:29 +0800

    添加bind函数模拟

--- call_apply.js
+++ call_apply.js
@@ -18,7 +18,7 @@
 // b.bar(); //2
 
 /**
- * 实现bind方法的思路
+ * 实现call方法的思路
  * 1. 将函数设为对象的属性	bar.fn = foo
  * 2. 执行该函数	bar.fn()
  * 3. 删除该函数	delete bar.fn
@@ -124,3 +124,79 @@ var d = [1, 2, 3];
 
 console.log(Math.max.newApply(null, d));
  */
+
+/**
+ * bind的模拟实现
+ * bind()方法会创建一个新函数。当这个新函数被调用时，bind()的第一个参数将作为它运行时的this，之后的一系列参数将会在传递的实参前作为它的参数
+ * bind函数的两个特点
+ * 1. 返回一个函数
+ * 2. 可以传入参数
+ */
+
+/**
+ * 第一版
+Function.prototype.newBind = function(context) {
+	var self = this;
+	//绑定函数可能是有返回值的所以return
+	return function() {
+		return self.apply(context);
+	};
+};
+ */
+
+/**
+ *第二版: 传入参数
+ *
+Function.prototype.newBind2 = function(context) {
+	var self = this;
+	// 获取newBind2函数从第二个参数到最后一个参数
+	var args = Array.prototype.slice.call(arguments, 1);
+
+	return function() {
+		// 这个时候的arguments是bind返回的函数传入的参数
+		var bindArgs = Array.prototype.slice.call(arguments);
+		return self.ally(context, args.concat(bindArgs));
+	};
+};
+ */
+
+/**
+ *第三版: 构造函数效果的模拟实现
+ *
+Function.prototype.newBind3 = function(context) {
+	var self = this;
+	var args = Array.prototype.slice.call(arguments, 1);
+
+	var fBound = function() {
+		var bindArgs = Array.prototype.slice.call(arguments);
+		// 当作为构造函数时，this指向实例，此时结果为true，将绑定函数的this指向该实例，可以让实例获得来自绑定函数的值
+		// 当作为普通函数时，this指向window，此时结果为false，将绑定函数的this指向context
+		return self.apply(this instanceof fBound ? this : context, args.concat(bindArgs));
+	};
+	// 修稿返回函数的prototype为绑定函数的prototype，实例就可以继承绑定函数中原型的值
+	fBound.prototype = this.prototype;
+	return fBound;
+};
+ */
+
+/**
+ * 第四版:构造函数效果的优化实现
+ *
+ */
+Function.prototype.newBind4 = function(context) {
+	if (typeof this !== 'function') {
+		throw new Error('Function.prototype.bind - what is trying to be bound is not callable');
+	}
+	var self = this;
+	var args = Array.prototype.slice.call(arguments, 1);
+
+	var fNOP = function() {};
+
+	var fBound = function() {
+		var bindArgs = Array.prototype.slice.call(arguments);
+		return self.apply(this instanceof fNOP ? this : context, args.concat(bindArgs));
+	};
+	fNOP.prototype = this.prototype;
+	fBound.prototype = new fNOP();
+	return fBound;
+};
